d80c673411439037570d215aa52cc3b4e34826e7
Arif Parjuni <arifparjjuni@gmail.com>
2020-11-23 15:05:23 +0700

    Penambahan folder project
    Semua file project sudah ditambhkan

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("Welcome Number Wizard, Bro");
+        Debug.Log("Pick an number, don't tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is: " + min);
+        Debug.Log("Tell me if your number is higher or lower than 500");
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed");
+        }
+
+        else if(Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Debug.Log("Down Arrow key was pressed");
+        }
+
+        else if(Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Enter key was pressed");
+        }
+
+    }
+}
