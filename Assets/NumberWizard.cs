62e4e71071b3c6a4ae334b8fba58bd507149e1aa
Arif Parjuni <arifparjjuni@gmail.com>
2020-11-23 17:23:07 +0700

    memindahkan variabel keluar vaoid
    memindahkan variabel ke min,max,guess ke luar void start dan update

--- Assets/NumberWizard.cs
+++ Assets/NumberWizard.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class NumberWizard : MonoBehaviour
 {
+
+    int max = 1000;
+    int min = 1;
+    int guess = 500;
+
     // Start is called before the first frame update
     void Start()
     {
-
-        int max = 1000;
-        int min = 1;
 
         Debug.Log("Welcome Number Wizard, Bro");
         Debug.Log("Pick an number, don't tell me what it is...");
@@ -25,11 +27,15 @@ public class NumberWizard : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.UpArrow))
         {
             Debug.Log("Up Arrow key was pressed");
+            min = guess;
+            Debug.Log(guess);
         }
 
         else if(Input.GetKeyDown(KeyCode.DownArrow))
         {
             Debug.Log("Down Arrow key was pressed");
+            max = guess;
+            Debug.Log(guess);
         }
 
         else if(Input.GetKeyDown(KeyCode.Return))
